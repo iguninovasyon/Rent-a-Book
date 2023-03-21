e3bf24e36dfd2578bae86cd7e50c87e5c618e123
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2023-03-21 20:22:33 +0300

    script layout created

---
+++ assets/scripts/index.js
@@ -0,0 +1,53 @@
+const users = [];
+const books = [];
+const leasedBooks = [];
+
+class User {
+    constructor(id,fullName,gender,status){
+        this.id = id
+        this.fullName = fullName
+        this.gender = gender
+        this.status = status
+    }
+    add() {
+        // Burak
+    }
+}
+
+class Book {
+    constructor(id,author,name, dailyPrice,status){
+        this.id = id
+        this.author = author
+        this.name = name
+        this.dailyPrice = dailyPrice
+        this.status = status
+    }
+    add() {
+        // Adem
+    }
+}
+
+class LeasedBooks {
+    constructor(id,book,day,user){
+        this.id = id
+        this.book = book
+        this.day = day
+        this.user = user
+        this.totalPrice = book.price * day
+    }
+    add() {
+        // Onur
+    }
+}
+
+function addUser() {
+
+}
+
+function addBook() {
+
+}
+
+function addRentABook() {
+
+}
