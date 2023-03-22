0a9af03c75e1ae4c6721383aee1657da7f43172d
ismailcankaratasss@gmail.com <ismailcankaratasss@gmail.com>
2023-03-22 22:16:48 +0300

    algorithm created

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,34 +2,46 @@ const users = [];
 const books = [];
 const leasedBooks = [];
 
-class User {
-    constructor(id,fullName,gender,status){
-        this.id = id
+class User { // Burak
+    constructor(fullName, gender, status = false){
+        this.id = randomId()
         this.fullName = fullName
         this.gender = gender
         this.status = status
     }
     add() {
-        // Burak
+        
+    }
+    update(id) {
+
+    }
+    get() {
+        // Tüm kullanıcıları getir.
     }
 }
 
-class Book {
-    constructor(id,author,name, dailyPrice,status){
-        this.id = id
+class Book {// Adem
+    constructor(author, name, dailyPrice, status = false){
+        this.id = randomId()
         this.author = author
         this.name = name
         this.dailyPrice = dailyPrice
         this.status = status
     }
     add() {
-        // Adem
+        
+    }
+    update(id) {
+        // Status değişme
+    }
+    get() {
+        // Tüm kitapları getir.
     }
 }
 
-class LeasedBooks {
-    constructor(id,book,day,user){
-        this.id = id
+class LeasedBooks { // Onur
+    constructor(book, day, user){
+        this.id = randomId()
         this.book = book
         this.day = day
         this.user = user
@@ -38,16 +50,39 @@ class LeasedBooks {
     add() {
         leasedBooks.push(this);
     }
+    get() {
+        // Tüm kiralanmış kitapları getir.
+    }
 }
 
-function addUser() {
-
+function randomId() {
+    // Random id üret.
+    // 1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed
+    let s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
-function addBook() {
-
+function addUser() { // Kullanıcı Kirala tıklandığında
+    // Form kontrol: tüm inputlar dolu mu?
+    // User oluştur. Tabloya Yazdır.
 }
 
-function addRentABook() {
+function addBook() { // Kitap Ekle tıklandığında
+    // Form kontrol: tüm inputlar dolu mu?
+}
 
+function addRentABook() { // Kitap Kirala tıklandığında
+    // kontrol: Kitap kiralanmış mı?, Kullanıcı kitap kiralamış mı?
+}
+
+function usersTableWrite(data) {
+    // Datayı tabloya yaz.
+}
+
+function booksTableWrite(data) {
+    // Datayı tabloya yaz.
+}
+
+function leasedBooksTableWrite(data) {
+    // Datayı tabloya yaz.
 }
